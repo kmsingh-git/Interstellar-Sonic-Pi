06254112dc68530783d3785f3d97b3cf12145093
Kanak Singh <kanaksingh@Kanaks-MacBook-Pro.local>
2022-03-19 23:47:39 -0400

    Added more notes. But sustain is muffling the sound. Is it a mastering issue?

--- Main.rb
+++ Main.rb
@@ -1,15 +1,28 @@
 # Welcome to Sonic Pi
 
+use_bpm 100
+
 octave = 4
-release = 2
-2.times do
-  play note(:C, octave: octave), release: release
-  play note(:E, octave: octave), release: release
-  sleep release
-  play note(:C, octave: octave), release: release
-  play note(:E, octave: octave), release: release
-  sleep release
-  play note(:C, octave: octave), release: release
-  play note(:E, octave: octave), release: release
-  sleep release+1
+
+attack_b = 0
+sustain_b = 2.4
+release_b = 0.6
+sleep_b = 1
+
+8.times do
+  ##| play note(:F, octave: octave), release: release
+  play note(:E, octave: octave), attack: attack_b, sustain: sustain_b, release: release_b
+  sleep sleep_b
 end
+
+##| 3.times do
+##|   play note(:E, octave: octave), attack: attack_b, sustain: sustain_b, release: release_b
+##|   play note(:A, octave: octave), attack: attack_b, sustain: sustain_b, release: release_b
+##|   sleep sleep_b
+##| end
+
+##| 3.times do
+##|   play note(:E, octave: octave), attack: attack_b, sustain: sustain_b, release: release_b
+##|   play note(:B, octave: octave), attack: attack_b, sustain: sustain_b, release: release_b
+##|   sleep sleep_b
+##| end
